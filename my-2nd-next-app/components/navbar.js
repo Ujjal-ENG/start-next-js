d2d81bd53e5668c739ad82ef15b85a3dcdc36f8b
user <ujjalroy7862@gmail.com>
2023-03-30 01:51:14 +0600

    navbar components make but it not show all the part of the app

---
+++ my-2nd-next-app/components/navbar.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <ul
+          className="menu-bar"
+          style={{
+            display: "flex",
+            justifyContent: "space-evenly",
+            margin: "20px",
+            fontSize: "24px",
+            fontWeight: "bold",
+          }}
+        >
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+
+          <Link href="/blog/blog1">Blog</Link>
+
+          <Link href="/product">Product</Link>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
--- my-2nd-next-app/pages/index.js
+++ my-2nd-next-app/pages/index.js
@@ -1,30 +1,11 @@
-import Link from "next/link";
+import Navbar from "@/components/navbar";
 import React from "react";
 
 const index = () => {
   return (
     <>
       <header>
-        <nav>
-          <ul
-            className="menu-bar"
-            style={{
-              display: "flex",
-              justifyContent: "space-evenly",
-              margin: "20px",
-              fontSize: "24px",
-              fontWeight: "bold",
-            }}
-          >
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
-
-            <Link href="/blog/blog1">Blog</Link>
-
-            <Link href="/product">Product</Link>
-          </ul>
-        </nav>
+        <Navbar />
       </header>
 
       <main></main>
