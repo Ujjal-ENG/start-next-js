4a54eda445db5ef191b6ccbd97c9a95db5aa3ff2
user <ujjalroy7862@gmail.com>
2023-03-30 01:01:09 +0600

    added the navbar on the next app

--- my-2nd-next-app/pages/index.js
+++ my-2nd-next-app/pages/index.js
@@ -1,10 +1,35 @@
+import Link from "next/link";
 import React from "react";
 
 const index = () => {
   return (
-    <div>
-      <h1>Hello Ujjal kopa</h1>
-    </div>
+    <>
+      <header>
+        <nav>
+          <ul
+            className="menu-bar"
+            style={{
+              display: "flex",
+              justifyContent: "space-evenly",
+              margin: "20px",
+              fontSize: "24px",
+              fontWeight: "bold",
+            }}
+          >
+            <Link href="/">Home</Link>
+            <Link href="/about">About</Link>
+            <Link href="/contact">Contact</Link>
+
+            <Link href="/blog/blog1">Blog</Link>
+
+            <Link href="/product">Product</Link>
+          </ul>
+        </nav>
+      </header>
+
+      <main></main>
+      <footer></footer>
+    </>
   );
 };
 
