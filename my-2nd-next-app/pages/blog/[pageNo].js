b80bf598b893e4c0b8db728f9772383a23ffab4a
user <ujjalroy7862@gmail.com>
2023-03-31 22:34:26 +0600

    based on the data dynamically i load the using fet static paths and get static props through

--- my-2nd-next-app/pages/blog/[pageNo].js
+++ my-2nd-next-app/pages/blog/[pageNo].js
@@ -1,12 +1,39 @@
-import { useRouter } from "next/router";
+import Navbar from "@/components/navbar";
+
 import React from "react";
 
-const pageNo = () => {
-  const router = useRouter();
-  const pageNumber = router.query.pageNo;
+export const getStaticPaths = async () => {
+  const fetchURL = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await fetchURL.json();
+  const paths = data.map((el) => {
+    return {
+      params: { pageNo: el.id.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.pageNo;
+  const fetchURL = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${id}`
+  );
+  const data = await fetchURL.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+const pageNo = ({ data }) => {
   return (
     <div>
-      <h1>this is dp {pageNumber} is kopa</h1>
+      <Navbar />
+      <h1>{data.body}</h1>
     </div>
   );
 };
