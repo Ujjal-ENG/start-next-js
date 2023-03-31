2b1307bac40731eda597e83106d2d774018de4bc
user <ujjalroy7862@gmail.com>
2023-03-31 21:41:49 +0600

    set the title of the each page and also set the meta information of the website for seo reanking purposes

--- my-2nd-next-app/pages/about.js
+++ my-2nd-next-app/pages/about.js
@@ -1,10 +1,19 @@
 import Navbar from "@/components/navbar";
 import React from "react";
 import Image from "next/image";
+import Head from "next/head";
 
 const about = () => {
   return (
     <>
+      <Head>
+        <meta charset="UTF-8" />
+        <meta name="description" content="Free Web tutorials" />
+        <meta name="keywords" content="HTML, CSS, JavaScript" />
+        <meta name="author" content="John Doe" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>About Page</title>
+      </Head>
       <Navbar />
       <div className="about">
         about
