79c60dc675831104133b2dca0f84850e614701e5
user <ujjalroy7862@gmail.com>
2023-03-31 21:51:40 +0600

    load the data from api and shown the data in a card also deisgn the card using styled components

--- my-2nd-next-app/pages/blog/blog1.js
+++ my-2nd-next-app/pages/blog/blog1.js
@@ -1,11 +1,44 @@
 import Navbar from "@/components/navbar";
 import React from "react";
-
-const blog1 = () => {
+export const getStaticProps = async () => {
+  const fetchURL = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await fetchURL.json();
+  return {
+    props: {
+      data,
+    },
+  };
+};
+const blog1 = ({ data }) => {
   return (
     <div>
       <Navbar />
-      blog1
+      {data.map((el) => {
+        return (
+          <div className="card" key={el.id}>
+            <h1>{el.id}</h1>
+            <p>{el.title}</p>
+          </div>
+        );
+      })}
+
+      <style jsx>
+        {`
+          .card {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            gap: 20px;
+            margin: 20px;
+            width: 600px;
+            border-radius: 10px;
+            padding: 20px;
+            cursor: pointer;
+            background-color: white;
+            color: black;
+          }
+        `}
+      </style>
     </div>
   );
 };
