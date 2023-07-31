7a811a587bea6745449ac299c96f6ac788af5667
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-07-31 11:20:56 +0600

    make the prorofile page

--- auth-nextjs/src/app/profile/[id]/page.tsx
+++ auth-nextjs/src/app/profile/[id]/page.tsx
@@ -1,5 +1,9 @@
-const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+const ProfilePage = ({ params }) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>ProfilePage Number: {params.id}</h1>
+    </div>
+  );
 };
 
 export default ProfilePage;
--- auth-nextjs/src/app/profile/page.tsx
+++ auth-nextjs/src/app/profile/page.tsx
@@ -1,5 +1,9 @@
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>ProfilePage</h1>
+    </div>
+  );
 };
 
 export default ProfilePage;
