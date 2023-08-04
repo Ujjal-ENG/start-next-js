2a7e1d8ba7638baf64e137e81496fde4daf25f45
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-08-05 00:25:22 +0600

    done thodays worked

--- ecommerce/app/layout.tsx
+++ ecommerce/app/layout.tsx
@@ -1,7 +1,11 @@
+import ModalProvider from "@/providers/modal-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -17,7 +21,10 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
