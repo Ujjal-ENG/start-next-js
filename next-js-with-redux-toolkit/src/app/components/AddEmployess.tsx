ab44c6e9f879ed0c42736189f934043ec86d686d
Ujjal-ENG <ujjalroy7862@gmail.com>
2025-06-03 16:08:43 +0600

    Add AddEmployees component and metadata for employee management

---
+++ next-js-with-redux-toolkit/src/app/components/AddEmployess.tsx
@@ -0,0 +1,12 @@
+const AddEmpolyees = () => {
+  return (
+    <div>
+      <h2> Add Employees</h2>
+    </div>
+  );
+};
+export default AddEmpolyees;
+export const metadata = {
+  title: "Add Employees",
+  description: "Add new employees to the system",
+};
