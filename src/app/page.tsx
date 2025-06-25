45470d6818e51e63384da041b120a8446f785494
Ujjal-ENG <ujjalroy7862@gmail.com>
2025-06-25 16:02:26 +0600

    Add Navbar component and integrate it into the home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
+import Navbar from "@/compoenents/navbar";
+
 export default function home() {
   return (
     <>
       <main>
-        <h1 className="text-3xl font-bold text-center mt-10">
-          Welcome to the Home Page
-        </h1>
-        <p className="text-center mt-4">
-          This is a simple home page built with Next.js.
-        </p>
+        <Navbar />
       </main>
     </>
   );
