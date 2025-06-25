90f691f33460664704e3ab1cb7a139a59f07d0a9
Ujjal-ENG <ujjalroy7862@gmail.com>
2025-06-25 17:15:22 +0600

    feat: add initial components configuration and utility functions
    - Created components.json for component configuration with Tailwind CSS and aliases.
    - Implemented OrgLandingPage with Navbar component.
    - Developed Button component with variants and sizes using class-variance-authority.
    - Added utility function `cn` for merging class names with Tailwind CSS.

---
+++ src/app/org/[slug]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Navbar from "@/compoenents/navbar";
+
+export default function OrgLandingPage() {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
+}
--- src/compoenents/navbar.tsx
+++ src/compoenents/navbar.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 const Navbar: React.FC = () => {
   return (
     <nav>
       <div className="border-b border-gray-200 bg-white p-4 flex justify-between items-center">
-        <h1>Multi Tenant Blog Application</h1>
-        <UserButton />
+        <div>
+          <h1 className="font-semibold text-xl">
+            Multi Tenant Blog Application
+          </h1>
+        </div>
+        <div className="flex items-center gap-4">
+          <OrganizationSwitcher afterSelectOrganizationUrl={"/org/:slug"} />
+          <UserButton />
+        </div>
       </div>
     </nav>
   );
