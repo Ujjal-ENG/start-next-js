5d4ce79b1e40247c31635b15975dea5649d1f68d
Ujjal-ENG <ujjalroy7862@gmail.com>
2025-06-26 15:16:08 +0600

    feat: implement Input and Textarea components for blog creation

--- src/app/org/[slug]/page.tsx
+++ src/app/org/[slug]/page.tsx
@@ -1,11 +1,25 @@
 "use client";
 
 import Navbar from "@/compoenents/navbar";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function OrgLandingPage() {
   return (
     <main>
       <Navbar />
+      <div>
+        <h1 className="font-medium text-xl">Create Your OWn Blog</h1>
+        <Input
+          className="max-w-3xl p-2 via-border m-2"
+          placeholder="Enter the Title here of the blog"
+        />
+        <Textarea className="max-w-3xl p-2 via-30% grayscale-50 m-2" />
+        <Button className="cursor-pointer p-2 via-30% grayscale-50 m-2">
+          Create Blog
+        </Button>
+      </div>
     </main>
   );
 }
