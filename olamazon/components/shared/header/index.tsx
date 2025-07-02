ef754cf1ce837c29c9ef0988cb1b86b2c2cdf1b5
Ujjal-ENG <ujjalroy7862@gmail.com>
2025-07-02 10:49:02 +0600

    refactor(Header): clean up imports and remove unused Sidebar component

--- olamazon/components/shared/header/index.tsx
+++ olamazon/components/shared/header/index.tsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
-import Link from "next/link";
-import Menu from "./menu";
-import Search from "./search";
-import data from "@/lib/data";
+import Image from 'next/image'
+import Link from 'next/link'
+import Menu from './menu'
+import Search from './search'
+import { APP_NAME } from '@/lib/constants'
+import data from '@/lib/data'
 
 export default async function Header() {
+  const t = (text: string) => text
+  const site = {
+    logo: '/icons/logo.svg',
+    name: APP_NAME,
+  }
   return (
     <header className="bg-black  text-white">
       <div className="px-2">
@@ -34,7 +40,6 @@ export default async function Header() {
         </div>
       </div>
       <div className="flex items-center px-3 mb-[1px]  bg-gray-800">
-        <Sidebar categories={categories} />
         <div className="flex items-center flex-wrap gap-3 overflow-hidden   max-h-[42px]">
           {data.headerMenus.map((menu) => (
             <Link
@@ -42,11 +47,11 @@ export default async function Header() {
               key={menu.href}
               className="header-button !p-2 "
             >
-              {t("Header." + menu.name)}
+              {t('Header.' + menu.name)}
             </Link>
           ))}
         </div>
       </div>
     </header>
-  );
+  )
 }
